6277201586128e7f31391ea68eb2d9080d134dde
jbisits <75812103+jbisits@users.noreply.github.com>
2022-06-14 08:36:18 +1000

    Update test_traceradvectiondiffusion.jl

--- test/test_traceradvectiondiffusion.jl
+++ test/test_traceradvectiondiffusion.jl
@@ -13,7 +13,7 @@ function test_constvel1D(stepper, dt, nsteps, dev::Device=CPU())
 
   prob = TracerAdvectionDiffusion.Problem(dev, true; nx, Lx, κ=0.0, u, dt, stepper, steadyflow=true)
   sol, cl, vs, pr, gr = prob.sol, prob.clock, prob.vars, prob.params, prob.grid
-  x = ArrayType(dev)(Array(gr.x))
+  x = ArrayType(dev)([gr.x[i] for i ∈ 1:gr.nx])
 
   σ = 0.1
   c0ampl = 0.1
@@ -51,7 +51,7 @@ function test_timedependentvel1D(stepper, dt, tfinal, dev::Device=CPU(); uvel = 
 
   prob = TracerAdvectionDiffusion.Problem(dev, true; nx, Lx, κ=0.0, u, dt, stepper)
   sol, cl, vs, pr, gr = prob.sol, prob.clock, prob.vars, prob.params, prob.grid
-  x = ArrayType(dev)(Array(gr.x))
+  x = ArrayType(dev)([gr.x[i] for i ∈ 1:gr.nx])
 
   σ = 0.2
   c0func(x) = 0.1 * exp(-(x^2) / (2σ^2))
@@ -162,7 +162,7 @@ function test_diffusion1D(stepper, dt, tfinal, dev::Device=CPU(); steadyflow = t
   prob = TracerAdvectionDiffusion.Problem(dev, true; steadyflow=steadyflow, nx=nx,
     Lx=Lx, κ=κ, dt=dt, stepper=stepper)
   sol, cl, vs, pr, gr = prob.sol, prob.clock, prob.vars, prob.params, prob.grid
-  x = ArrayType(dev)(Array(gr.x))
+  x = ArrayType(dev)([gr.x[i] for i ∈ 1:gr.nx])
   
   c0ampl, σ₀ = 0.1, 0.1
   σ(t) = sqrt(2κ * t + σ₀)
